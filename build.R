3feea22ebed42258b1057f729c6830023194a07b
Yihui Xie <xie@yihui.name>
2018-06-07 16:44:19 -0500

    automatically install brew dependencies

--- build.R
+++ build.R
@@ -1,20 +1,31 @@
 ver = unlist(getRversion())[1:2]  # version x.y
 dir = file.path('bin/macosx/el-capitan/contrib', paste(ver, collapse = '.'))
+# install brew dependencies
+install_dep = function(pkg) {
+  dep = c(
+    RGtk2 = 'gtk+',
+    RProtoBuf = 'protobuf'
+  )[pkg]
+  if (!is.na(dep)) system(paste('brew install', dep))
+}
 
 # install xfun (from Github)
 if (!requireNamespace('xfun', quietly = TRUE) || packageVersion('xfun') < '0.1.10') {
   source('https://install-github.me/yihui/xfun')
 }
 
+# TODO: remove this when RGtk2 > 2.20.34 is on CRAN
 if (!xfun::loadable('devtools', new_session = TRUE)) install.packages('devtools')
 if (!xfun::loadable('RGtk2', new_session = TRUE)) {
-  system('brew install gtk+')
+  install_dep('RGtk2')
   devtools::install_github('lawremi/RGtk2/RGtk2')
 }
 
 # make sure these packages' dependencies are installed
 for (pkg in pkgs <- readLines('packages')) {
-  if (!xfun::loadable(pkg, new_session = TRUE)) install.packages(pkg)
+  if (!xfun::loadable(pkg, new_session = TRUE)) {
+    install_dep(pkg); install.packages(pkg)
+  }
 }
 
 system('git checkout gh-pages')
@@ -33,6 +44,7 @@ pkgs = intersect(pkgs, db[, 'Package'])
 
 if (length(pkgs) == 0) q('no')
 
+# TODO: remove this when RGtk2 > 2.20.34 is on CRAN
 if ('RGtk2' %in% pkgs && db['RGtk2', 'Version'] == '2.20.34') {
   system('brew install gtk+')
   system('git clone --depth=1 https://github.com/lawremi/RGtk2.git')
@@ -45,6 +57,7 @@ for (pkg in pkgs) xfun:::download_tarball(pkg, db)
 
 # build binary packages
 for (pkg in list.files('.', '.+[.]tar[.]gz$')) {
+  install_dep(gsub('_.*$', '', pkg))
   if (xfun::Rcmd(c('INSTALL', '--build', pkg)) != 0) stop(
     'Failed to build the package ', pkg
   )
