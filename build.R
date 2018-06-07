81566121937abd957269abd870b11d545975ef03
Yihui Xie <xie@yihui.name>
2018-06-07 01:14:12 -0500

    a more reliable way to determine which packages need to be rebuilt to binaries (compare the versions in PACKAGES against CRAN versions)

--- build.R
+++ build.R
@@ -1,30 +1,27 @@
 ver = unlist(getRversion())[1:2]  # version x.y
 dir = file.path('bin/macosx/el-capitan/contrib', paste(ver, collapse = '.'))
-dir.create(dir, recursive = TRUE, showWarnings = FALSE)
 
 # install xfun (from Github)
 if (!requireNamespace('xfun') || packageVersion('xfun') < '0.1.10') {
   source('https://install-github.me/yihui/xfun')
 }
 
-owd = setwd(tempdir())
-
 # make sure these packages' dependencies are installed
 for (pkg in pkgs <- readLines('packages')) {
   if (!xfun::loadable(pkg, new_session = TRUE)) install.packages(pkg)
 }
 
+system('git checkout gh-pages')
 # download source packages that have been updated on CRAN
-if (file.exists(file.path(owd, dir, 'PACKAGES'))) {
-  old = old.packages(.libPaths()[1], checkBuilt = TRUE, type = 'source')
-  colnames(old)[5] = 'Version' # ReposVer
-} else {
-  old = available.packages(type = 'source')  # no binaries have been built
+db = available.packages(type = 'source')
+if (file.exists(pkg_file <- file.path(dir, 'PACKAGES'))) {
+  info = read.dcf(pkg_file, c('Package', 'Version'))
+  pkgs = intersect(pkgs, info[as.numeric_version(db[info[, 1], 'Version']) > info[, 2], 1])
 }
-if (!is.matrix(old)) q('no')
 
-for (pkg in intersect(pkgs, old[, 'Package'])) xfun:::download_tarball(pkg, old)
-if (is.null(pkg)) q('no')
+if (length(pkgs) == 0) q('no')
+
+for (pkg in pkgs) xfun:::download_tarball(pkg, db)
 
 # build binary packages
 for (pkg in list.files('.', '.+[.]tar[.].gz$')) {
@@ -32,7 +29,10 @@ for (pkg in list.files('.', '.+[.]tar[.].gz$')) {
     'Failed to build the package ', pkg
   )
 }
-file.copy(list.files('.', '.+[.]tgz$'), file.path(owd, dir), overwrite = TRUE)
-setwd(owd)
+
+dir.create(dir, recursive = TRUE, showWarnings = FALSE)
+
+file.copy(list.files('.', '.+[.]tgz$'), dir, overwrite = TRUE)
+unlink(c('*.tar.gz', '*.tgz'))
 
 tools::write_PACKAGES(dir, type = 'mac.binary')
--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 set -e
 
 git fetch origin gh-pages:gh-pages
-git checkout gh-pages
 
 Rscript build.R
 
 [ "${TRAVIS_PULL_REQUEST}" != "false" ] && exit 0
 
+git checkout gh-pages
 git add -A
 git commit --amend -m"Publishing from Travis build $TRAVIS_BUILD_NUMBER"
 git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git origin gh-pages > /dev/null
